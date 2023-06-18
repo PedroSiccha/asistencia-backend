6a6681f38467f2ca48b913bf6f6f6d85e3830d0b
Pedro Diaz <pedro.d.siccha@gmail.com>
2023-06-17 20:19:04 -0500

    View PORT

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
+  console.log("PORT", process.env.PORT_SERVICE);
   await app.listen(process.env.PORT_SERVICE || 3000, process.env.SERVICE_URL || '127.0.0.1');
 }
 bootstrap();
